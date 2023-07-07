d60f724012334648e478ccd5abbc95657d6a0759
Cole Phillips <colephillips1@gmail.com>
2023-07-07 17:51:04 -0400

    Add exercise solutions

--- javascript/functions/07_11_Function_Exercises/app.js
+++ javascript/functions/07_11_Function_Exercises/app.js
@@ -104,4 +104,117 @@ console.log(singleLetterCount("hello", "h"));
 
 prompt();
 
+// this function takes in one parameter (a string) and returns an object with the keys being the letters and the values being the count of the letter.
 
+function multipleLetterCount(str) {
+  str = str.toLowerCase();
+  let finalObj = {};
+  for (let i = 0; i < str.length; i++) {
+    if (finalObj[str[i]] === undefined) {
+      finalObj[str[i]] = 1;
+    } else {
+      finalObj[str[i]]++;
+    }
+  }
+  return finalObj;
+}
+
+multipleLetterCount("hello"); // {h:1, e: 1, l: 2, o:1}
+multipleLetterCount("person"); // {p:1, e: 1, r: 1, s:1, o:1, n:1}
+
+prompt();
+
+/*
+this function should take in at most four parameters (an array, command, location, and value).
+	If the command is “remove” and the location is “end”, the function should remove the last value in the array and return the value removed. (In this case, the function only needs three arguments.)
+	If the command is “remove” and the location is “beginning”, the function should remove the first value in the array and return the value removed. (In this case, the function only needs three arguments.)
+	If the command is “add” and the location is “beginning”, the function should add the value (fourth parameter) to the beginning of the array and return the array.
+	If the command is “add” and the location is “end”, the function should add the value (fourth parameter) to the end of the array and return the array.
+*/
+
+function arrayManipulation(arr, command, position, val) {
+  if (command === "remove") {
+    if (position === "end") {
+      return arr.pop();
+    }
+    return arr.shift();
+  } else if (command === "add") {
+    if (position === "end") {
+      arr.push(val);
+      return arr;
+    }
+    arr.unshift(val);
+    return arr;
+  }
+}
+
+console.log(arrayManipulation([1, 2, 3], "remove", "end")); // 3)
+console.log(arrayManipulation([1, 2, 3], "remove", "beginning")); // 1)
+console.log(arrayManipulation([1, 2, 3], "add", "beginning", 20)); // [20,1,2,3]
+console.log(arrayManipulation([1, 2, 3], "add", "end", 30)); // [1,2,3,30]
+
+prompt();
+// A Palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward. This function should take in one parameter and returns true or false if it is a palindrome. As a bonus, allow your function to ignore whitespace and capitalization so that isPalindrome(‘a man a plan a canal Panama’); returns true
+
+/*
+
+Problem:
+Understand and restate
+
+Input: string
+Output: string (shorter than input string)
+
+Explicit requirements:
+Return a new string
+Returned string has some elements removed
+
+Implicit & questions
+Can we mutate the original string, or should we return a new string?
+Does it have to be a valid word?
+What is a word?
+Are numbers a word?
+Do we maintain punctuation?
+Which word is “every other”? Second word
+
+Mental model
+Receive a string, work through it, when I see whitespace, remove a word
+
+Examples
+
+removeWords(“here in the breakout room”) // “here the room”
+removeWords(“”) // “”
+removeWords(“hello”) // “hello”
+removeWords(“are we counting, punctuation?”) // “are counting,”
+removeWords(“hello 5 world”) // “hello 5” (numbers aren’t words)
+
+Data structures
+arrays
+
+E)
+    removeEveryOtherWord('Cow jumped over the moon') // ('Cow over moon')
+    removeEveryOtherWord('Cow jump') // ('Cow')
+    removeEveryOtherWord('Cow') // ('Cow')
+    removeEveryOtherWord('') //
+    removeEveryOtherWord('Cow jumped over the') // ('Cow over')
+    removeEveryOtherWord('A cow jumped over the') // ('A jumped the')
+
+D) array
+
+A)
+    first split the string into an array of words, identify the odd indexes and remove them from the array and then return the reassembled string.
+*/
+
+function isPalindrome(string) {
+  let array = string.split("");
+  let reversedArray = array.reverse();
+  let reversedText = reversedArray.join("");
+  return string === reversedText;
+}
+
+isPalindrome("testing"); // false
+
+isPalindrome("tacocat"); // true
+
+isPalindrome("hannah"); // true
+
+isPalindrome("robert"); // false
