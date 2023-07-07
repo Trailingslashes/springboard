f0985c838f0e17fe45918a0d2bb1dc23b263f77e
Cole Phillips <colephillips1@gmail.com>
2023-07-07 12:39:32 -0400

    Add exercise solution

--- javascript/functions/07_10_Practice_4_Pangrams/app.js
+++ javascript/functions/07_10_Practice_4_Pangrams/app.js
@@ -2,6 +2,15 @@
 //"The quick brown fox jumps over the lazy dog"
 
 // Write a function called isPangram, which checks to see if a given sentence contains every letter of the alphabet.  Make sure you igore string casing!
+function isPangram(sentence) {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  for (let i = 0; i < alphabet.length; i++) {
+    if (!sentence.toLowerCase().includes(alphabet[i])) {
+      return false;
+    }
+  }
+  return true;
+}
 
-// isPangram('The five boxing wizards jump quickly') //true
-// isPangram('The five boxing wizards jump quick') //false
+console.log(isPangram("The five boxing wizards jump quickly")); //true
+console.log(isPangram("The five boxing wizards jump quick")); //false
