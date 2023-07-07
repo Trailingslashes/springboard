938279440871f694a3c12f2f3058dadf22e41e4b
Cole Phillips <colephillips1@gmail.com>
2023-07-07 11:36:33 -0400

    Add exercise solution

--- javascript/functions/07_07_Practice_1_Password_Validator/app.js
+++ javascript/functions/07_07_Practice_1_Password_Validator/app.js
@@ -7,6 +7,36 @@
 // If all requirements are met, return true.
 //Otherwise: false
 
-// isValidPassword('89Fjj1nms', 'dogLuvr');  //true
-// isValidPassword('dogLuvr123!', 'dogLuvr') //false
-// isValidPassword('hello1', 'dogLuvr') //false
+// function isValidPassword(password, username) {
+//   if (username !== password) {
+//     return /^(?!.*\b(?:dogLuvr|${username})\b)(?=.*[^\s]).{8,}$/.test(password);
+//   }
+// }
+
+// function isValidPassword(password, username) {
+//   if (password.length < 8) {
+//     return false;
+//   }
+//   if (password.indexOf(" ") !== -1) {
+//     return false;
+//   }
+//   if (password.indexOf(username) !== -1) {
+//     return false;
+//   }
+//   return true;
+// }
+
+const isValidPassword = (password, username) => {
+  if (
+    password.length < 8 ||
+    password.indexOf(" ") !== -1 ||
+    password.indexOf(username) !== -1
+  ) {
+    return false;
+  }
+  return true;
+};
+
+console.log(isValidPassword("89Fjj1nms", "dogLuvr"));
+console.log(isValidPassword("dogLuvr123!", "dogLuvr"));
+console.log(isValidPassword("hello1", "dogLuvr"));
