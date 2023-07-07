c8abed227ad15e8ddffb955339e6be3d710c4be4
Cole Phillips <colephillips1@gmail.com>
2023-07-07 13:16:04 -0400

    Add exercise solution

--- javascript/functions/07_09_Practice_3_getCard/app.js
+++ javascript/functions/07_09_Practice_3_getCard/app.js
@@ -8,3 +8,28 @@
 //Pick a random suit from:
 //----clubs,spades, hearts, diamonds
 //Return both in an object
+
+function getCard() {
+  cards = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A",
+  ];
+  suit = ["clubs", "spades", "hearts", "diamonds"];
+  const randomCard = cards[Math.floor(Math.random() * cards.length)];
+  const randomSuit = suit[Math.floor(Math.random() * suit.length)];
+  return { value: randomCard, suit: randomSuit };
+}
+
+console.log(getCard());
