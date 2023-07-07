701b0e51362b8664e5fe882cce5c35ce5675e288
Cole Phillips <colephillips1@gmail.com>
2023-07-07 12:11:38 -0400

    Add exercise solution

--- javascript/functions/07_08_Practice_2_Avg/app.js
+++ javascript/functions/07_08_Practice_2_Avg/app.js
@@ -1,3 +1,13 @@
 // Write a function to find the average value in an array of numbers
-//avg([0,50]) //25
-//avg([75,76,80,95,100]) //85.2
+
+function avg(arr) {
+  let sum = 0;
+  arr.forEach((num) => {
+    sum += num;
+  });
+  return Math.floor(sum / arr.length);
+}
+
+console.log(avg([0, 50])); //25
+console.log(avg([75, 76, 80, 95, 100])); //85.2
+console.log(avg([100, 200, 234, 343, 200, 403]));
