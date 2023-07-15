5970bdf66877a729930795a335078c09d3c3cd28
Cole Phillips <colephillips1@gmail.com>
2023-07-15 18:03:10 -0400

    Add exercise solution

--- dom_manipulation/todo_app/app.js
+++ dom_manipulation/todo_app/app.js
@@ -1,3 +1,24 @@
-const addToDo = document.getElementById("add-todo");
+const submit = document.getElementById("button");
+const inputValue = document.getElementById("todo");
+const newToDo = document.getElementById("tasks");
 
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (inputValue.value === "") {
+    alert("please add a todo");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputValue.value;
+    localStorage.setItem("Task name:", JSON.stringify(inputValue.value));
+    newToDo.appendChild(li);
+    inputValue.value = "";
 
+    li.addEventListener("click", () => {
+      li.classList.add("selected");
+    });
+
+    li.addEventListener("dblclick", () => {
+      li.remove();
+    });
+  }
+});
